4d442e22eadc7e115a0a4d19ed5bc623d7ea77fc
Druthy <martinhoride@gmail.com>
2020-10-10 22:45:36 +1300

    Re Upload
    Re upload of project with better name and file structure.
    
    Rename again, commit failed because file names too long.

---
+++ Fire Solution/Assets/Scripts/WireFrame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireFrame : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public Mesh mesh;
+    public Material material;
+    public Transform objectTransform;
+
+    public void Update()
+    {
+        // will make the mesh appear in the Scene at origin position
+        Graphics.DrawMesh(mesh, objectTransform.position, objectTransform.rotation, material, 0);
+    }
+}
---
+++ Fire Solution/Assets/Scripts/WireFrameImproved.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireFrameImproved : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public Material material;
+    public Mesh mesh;
+
+    public void Update()
+    {
+        // will make the mesh appear in the Scene at origin position
+        Graphics.DrawMesh(mesh, this.transform.position, this.transform.rotation, material, 0);
+    }
+}
