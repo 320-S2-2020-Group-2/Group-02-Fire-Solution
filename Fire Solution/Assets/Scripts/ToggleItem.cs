bddc0c591acb40de363d8a4bcdc05ad4f7c1fee7
Druthy <martinhoride@gmail.com>
2020-10-23 10:21:45 +1300

    Toggle Script
    Toggle script created to add make buttons toggle.
    theres probably an easier way but oh well

---
+++ Fire Solution/Assets/Scripts/ToggleItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleItem : MonoBehaviour
+{
+    public bool toggleVariable = true;
+    void toggleActive()
+    {
+        if (this.toggleVariable == true) {
+            this.toggleVariable = false;
+        }
+        else if (this.toggleVariable == false) {
+            this.toggleVariable = true;
+        }
+    }
+}
