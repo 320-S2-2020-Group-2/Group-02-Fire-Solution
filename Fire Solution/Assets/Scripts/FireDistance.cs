927460378517a0b6a2815bdc3c3f2136156508b8
Druthy <martinhoride@gmail.com>
2020-10-25 11:47:48 +1300

    Distance Alerts
    Pop up now displays at a certain distance from a fire

---
+++ Fire Solution/Assets/Scripts/FireDistance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FireDistance : MonoBehaviour
+{
+    public GameObject alert;
+    public GameObject fireSource;
+    public GameObject mainCamera;
+    public float alertDistance = (float) 3;
+
+    void Update()
+    {
+        float dist = Vector3.Distance(mainCamera.transform.position, fireSource.transform.position);
+        if (dist > alertDistance)
+        {
+            alert.SetActive(false);
+        }
+        else
+        {
+            alert.SetActive(true);
+        }
+    }
+}
