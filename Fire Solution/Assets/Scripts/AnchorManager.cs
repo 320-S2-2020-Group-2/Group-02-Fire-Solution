8c786115f3fc98f591ce68c897efa326cd683358
Druthy <martinhoride@gmail.com>
2020-10-24 23:42:06 +1300

    Spatial Anchor Mockup
    Menu Button to create anchor
    Menu Button to remove anchors
    click to change anchor type

---
+++ Fire Solution/Assets/Scripts/AnchorManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorManager : MonoBehaviour
+{
+    public GameObject AnchorItem;
+    public Camera mainCamera;
+
+    public void NewAnchor()
+    {
+        Vector3 newPosition = new Vector3 (mainCamera.transform.position.x, mainCamera.transform.position.y - (float) 0.5, mainCamera.transform.position.z);
+        GameObject newRotation = new GameObject();
+        newRotation.transform.rotation = new Quaternion (mainCamera.transform.rotation.x, 0, mainCamera.transform.rotation.z, 1);
+
+
+        GameObject newAnchorItem = Instantiate(AnchorItem, newPosition, mainCamera.transform.rotation);
+    }
+    public void ClearAnchors()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Anchor");
+
+        for (int i=0; i < objects.Length; i++)
+        {
+            Destroy(objects[i]);
+        }
+
+    }
+
+}
---
+++ Fire Solution/Assets/Scripts/ClickTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClickTest : MonoBehaviour
+{
+    public GameObject anchor;
+    public Material material;
+    public TextMeshPro textDescription;
+    private bool hazard = true;
+
+    public void toggleType()
+    {
+        anchor.GetComponent<MeshRenderer>().material = material;
+        switchMaterial();
+    }
+    void switchMaterial()
+    {
+        if (hazard == true)
+        {
+            hazard = false;
+            material.SetColor("_Color", Color.green);
+            textDescription.SetText("Objective");
+        }
+        else if (hazard == false)
+        {
+            hazard = true;
+            material.SetColor("_Color", Color.magenta);
+            textDescription.text = "Hazard";
+        }
+    }
+
+}
