fb48f0a813398bf7e0e144d1cd52b848e03f56fc
Druthy <martinhoride@gmail.com>
2020-10-25 17:02:58 +1300

    API Call
    Fire summary information is given based on a call to an API

---
+++ Fire Solution/Assets/Scripts/APIManager.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+public class APIManager : MonoBehaviour
+{
+    public GameObject fireTemplate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        JToken fighters = CallAPI("https://prod-12.australiasoutheast.logic.azure.com:443/workflows/954e7fbaeb3c4947819ba0823d5d5999/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=tjz08Xt2xdzVK61RysoFF7npUFxIFumx9VnNuQCik-o");
+        string fighterString = "";
+        foreach (var fighter in fighters)
+        {
+            fighterString += fighter["Name"].ToString() + ", ";
+        }
+        fighterString = fighterString.Substring(0, fighterString.Length - 2);
+        GameObject fireObject = Instantiate(fireTemplate, new Vector3(0f, 4f, -8f), Quaternion.identity);
+        FireManager fireManager = fireObject.GetComponent<FireManager>();
+
+        fireManager.mrtkToolTip.ToolTipText = fighterString;
+        //fireManager.CrteateHammer();
+    }
+
+    // Update is called once per frame
+    public JToken CallAPI(string uri)
+    {
+        HttpWebRequest requestHandler = (HttpWebRequest)WebRequest.Create(uri);
+        HttpWebResponse responseHandler = (HttpWebResponse)requestHandler.GetResponse();
+
+        StreamReader reader = new StreamReader(responseHandler.GetResponseStream());
+        string response = reader.ReadToEnd();
+
+        Debug.Log(response);
+
+        JToken jsonResponse = JToken.Parse(response);
+
+        return jsonResponse;
+    }
+}
---
+++ Fire Solution/Assets/Scripts/FireManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FireManager : MonoBehaviour
+{
+    //public TextMeshPro tooltip;
+    public ToolTip mrtkToolTip;
+    public GameObject hammer;
+    //public float gravity;
+
+    /*public void CrteateHammer()
+    {
+        GameObject theHammer = GameObject.Instantiate(hammer);
+        theHammer.GetComponent<Rigidbody>().AddForce(new Vector3(0f, -gravity, 0f));
+    }*/
+}
